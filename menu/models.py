06afc6c4c53dd4c15a994463affada35b0b994c0
ssbelousov <ss.belousov@yandex.ru>
2023-04-06 16:43:37 +0300

    Тестовое задание

--- menu/models.py
+++ menu/models.py
@@ -5,8 +5,12 @@ class MenuItem(models.Model):
     title = models.CharField(max_length=100)
     url = models.CharField(max_length=100, blank=True)
     named_url = models.CharField(max_length=50, blank=True)
-    parent = models.ForeignKey(
-        'self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', 
+                               blank=True, null=True, 
+                               related_name='children', 
+                               on_delete=models.CASCADE,
+                               )
 
     def __str__(self):
         return self.title
+    
--- menu/templatetags/menu_tags.py
+++ menu/templatetags/menu_tags.py
@@ -4,33 +4,33 @@ from ..models import MenuItem
 
 register = template.Library()
 
-
 @register.simple_tag(takes_context=True)
 def draw_menu(context, menu_name):
     request = context['request']
     current_url = resolve(request.path_info).url_name
-
     menu_items = MenuItem.objects.filter(title=menu_name)
-
     menu_html = '<ul>'
+    
     for item in menu_items:
-        if item.parent is None:
+        if not item.parent:
             menu_html += render_menu_item(item, current_url)
             menu_html += '<ul>'
+            
             for child_item in item.children.all():
+                menu_html += '<li>'
                 menu_html += render_menu_item(child_item, current_url)
                 menu_html += '<ul>'
+                
                 for grandchild_item in child_item.children.all():
                     menu_html += render_menu_item(grandchild_item, current_url)
-                menu_html += '</ul>'
-            menu_html += '</li></ul>'
+                
+                menu_html += '</ul></li>'
+            
+            menu_html += '</ul></li>'
+    
     menu_html += '</ul>'
-
     return menu_html
 
-
 def render_menu_item(item, current_url):
-    active = ''
-    if item.url == current_url or item.named_url == current_url:
-        active = 'active'
+    active = 'active' if item.url == current_url or item.named_url == current_url else ''
     return f'<li class="{active}\"><a href="{item.url or reverse(item.named_url)}">{item.title}</a></li>'
